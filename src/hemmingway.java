93114a43348fde2243ab669491db0c00f5958c01
derhaideer2 <muhammedemir.akguel@gmail.com>
2025-06-07 20:06:00 +0200

    Solving 2 new Problems

--- src/hemmingway.java
+++ src/hemmingway.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
 public class hemmingway {
+
+    public static void main(String[] args){
+        int x = 11;
+        int y = 213908932;
+        int z = (int)Math.pow(2, 31) ;
+
+        System.out.println(
+                  "HW von " + x + " = " + hammingWeight(x) +
+                "\nHW von " + y + " = " + hammingWeight(y) +
+                "\nHM von " + z + " = " + hammingWeight(z)
+        );
+
+    }
+    public static int hammingWeight(int n) {
+        int counter = 0;
+        int numberOfBitsInPositivInt = 31; //2-er Komplement int hat 32, aber most
+                                           //sig bit ist für neg Zahlen
+        for(int i = 1; i <= numberOfBitsInPositivInt; i++){
+            if((n & 1) == 1){
+                counter = counter + 1;
+                n = n>>1;
+            }else{
+                n = n>>1;
+            }
+        }
+        return counter;
+    }
 }
--- src/twoSum.java
+++ src/twoSum.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
 
 public class twoSum {
+        public int[] twoSum(int[] nums, int target) {
+            HashMap<Integer, Integer> map = new HashMap<>();
+            int[] resultArr = new int[2];
+            for(int i = 0; i < nums.length; i++){
+                int gesucht = target - nums[i];
+                if(map.containsKey(gesucht)){
+                    resultArr[0] = map.get(gesucht);
+                    resultArr[1] = i;
+                    return resultArr;
+
+                }else{
+                    map.put(nums[i], i);
+                }
+            }
+            return resultArr;
+        }
 }
